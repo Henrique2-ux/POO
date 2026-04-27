1f7f9643303af10740bbd2d46bb6f4c7c20d9d0c
Arthur Henrique <arthur.ribas456@gmail.com>
2026-04-27 08:38:46 -0400

    Update EXERCICIO_VOTOS_NOMINAIS_PROFESSOR.cpp

--- EXERCICIO_VOTOS_NOMINAIS_PROFESSOR.cpp
+++ EXERCICIO_VOTOS_NOMINAIS_PROFESSOR.cpp
@@ -2,85 +2,103 @@
 #include <string>
 #include <list>
 #include <map> 
-
 using namespace std;
-class pleito;
+
+// DECLARAÇÕES FORWARD (corrigidas)
 class pessoa;
 class eleitor;
 class candidato;
+class pleito;
 
-class pessoa{
-    public:
-        string nome = "";
-        pessoa(string nome){
-            this->nome = nome;
-        }
+// CLASSE BASE
+class pessoa {
+public:
+    string nome;
+    // CONSTRUTOR PADRÃO (obrigatório para list)
+    pessoa() : nome("") {}
+    pessoa(string n) : nome(n) {}
 };
 
-class eleitor : public pessoa{
-    public:
-        int titulo = 0;
-        eleitor(string n) : pessoa(n){
-        }
+// ELEITOR HERDA DE PESSOA
+class eleitor : public pessoa {
+public:
+    int titulo = 0;
+    eleitor() {}
+    eleitor(string n) : pessoa(n) {}
 };
 
-class candidato : public pessoa{
-    public:
-        pleito* eleicao;
-        candidato(string n) : pessoa(n){
-
-        }
+// CANDIDATO HERDA DE PESSOA
+class candidato : public pessoa {
+public:
+    candidato() {}
+    candidato(string n) : pessoa(n) {}
 };
 
-class voto{
-    public: 
-        candidato* candidatoEscolhido;
-        eleitor* votoEleitor;
-        voto(candidato* c, eleitor* e){
-            candidatoEscolhido = c;
-            votoEleitor = e;
-        }
+// VOTO (associação entre eleitor e candidato)
+class voto {
+public: 
+    candidato* candidatoEscolhido;
+    eleitor* eleitor;
+    
+    voto(candidato* c, eleitor* e) : candidatoEscolhido(c), eleitor(e) {}
 };
 
-class pleito{
-    public:
-        list<voto> votos;
-        list<candidato> candidatos;
+// ELEIÇÃO (contém listas)
+class pleito {
+public:
+    list<voto> votos;
+    list<candidato> candidatos;  // ARMAZENA OBJETOS (não ponteiros)
 };
 
-void apuracao(pleito eleicao){
-    map<candidato*, int> contagem;
-    for(candidato c : eleicao.candidatos){
-        contagem[&c] = 0;
+// FUNÇÃO APURAÇÃO CORRIGIDA
+void apuracao(const pleito& eleicao) {  // PASSA POR REFERÊNCIA
+    map<string, int> contagem;  // CHAVE: NOME (string) ao invés de ponteiro
+    
+    // INICIALIZA CONTAGEM COM CANDIDATOS
+    for(const auto& c : eleicao.candidatos) {
+        contagem[c.nome] = 0;
     }
-
-    for(voto v : eleicao.votos){
-        contagem[v.candidatoEscolhido]++;
+    
+    // CONTA VOTOS
+    for(const auto& v : eleicao.votos) {
+        contagem[v.candidatoEscolhido->nome]++;
     }
-    for(auto c : contagem)
-    cout << c.first->nome << ":" << c.second << endl;
-};
-
+    
+    // EXIBE RESULTADO
+    cout << "\n=== RESULTADO DA ELEIÇÃO ===" << endl;
+    for(const auto& par : contagem) {
+        cout << par.first << ": " << par.second << " votos" << endl;
+    }
+}
 
 int main() {
-    std::cout << "eleicao 26";
-
+    cout << "=== Eleição 2026 ===" << endl;
+    
+    // CRIANDO ELEIÇÃO
     pleito eleicaosindicato;
+    
+    // ELEITORES
     eleitor m1("Jose");
-    eleitor m2("Joao");
-    eleitor m3("maria");
-    eleitor m4("marta");
-
-    candidato c1("jose");
-    candidato c2("maria");
-
+    eleitor m2("Joao"); 
+    eleitor m3("Maria");
+    eleitor m4("Marta");
+    
+    // CANDIDATOS
+    candidato c1("Jose");
+    candidato c2("Maria");
+    
+    // ADICIONANDO CANDIDATOS À ELEIÇÃO
     eleicaosindicato.candidatos.push_front(c1);
     eleicaosindicato.candidatos.push_front(c2);
     
-    eleicaosindicato.votos.push_front(voto(&c1, &m1));
-    eleicaosindicato.votos.push_front(voto(&c2, &m2));
-    eleicaosindicato.votos.push_front(voto(&c2, &m3));
-    eleicaosindicato.votos.push_front(voto(&c1, &m4));
+    // REGISTRANDO VOTOS
+    eleicaosindicato.votos.push_front(voto(&c1, &m1)); // Jose vota Jose
+    eleicaosindicato.votos.push_front(voto(&c2, &m2)); // Joao vota Maria
+    eleicaosindicato.votos.push_front(voto(&c2, &m3)); // Maria vota Maria
+    eleicaosindicato.votos.push_front(voto(&c1, &m4)); // Marta vota Jose
+    
+    // APURANDO RESULTADO
+    apuracao(eleicaosindicato);
     
     return 0;
 }
